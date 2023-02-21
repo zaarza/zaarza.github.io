b67c89ceb6da9c5bcf53b4c8f4a85ac958662b0d
zaarza <zaarza03@gmail.com>
2023-02-21 11:13:07 +0700

    Adding new project

--- src/data/getProjects.jsx
+++ src/data/getProjects.jsx
@@ -2,8 +2,21 @@ function getProjects() {
   return [
     {
       id: 'project-1',
-      name: 'Simple Note App',
+      name: 'Rest Countries App',
       imageUrl: '/assets/images/project-1.png',
+      description: 'A challenge from Frontendmentor',
+      liveViewUrl: 'https://rest-countries-app-zaarza.vercel.app/',
+      repositoryUrl: 'https://github.com/zaarza/rest-countries-app',
+      technology: [
+        { name: 'ReactJS' },
+        { name: 'Redux' },
+        { name: 'SASS' },
+      ],
+    },
+    {
+      id: 'project-2',
+      name: 'Simple Note App',
+      imageUrl: '/assets/images/project-2.png',
       description: 'Submission for Dicoding course "Belajar Fundamental Aplikasi Web dengan React"',
       liveViewUrl: 'https://simple-note-app-zaarza.vercel.app/',
       repositoryUrl: 'https://github.com/zaarza/simple-note-app',
@@ -13,9 +26,9 @@ function getProjects() {
       ],
     },
     {
-      id: 'project-2',
+      id: 'project-3',
       name: 'Restaurant Catalogue',
-      imageUrl: '/assets/images/project-2.png',
+      imageUrl: '/assets/images/project-3.png',
       description: 'Submission for Dicoding course "Menjadi Front-End Web Developer Expert"',
       liveViewUrl: 'https://restaurant-catalogue-zaarza.vercel.app/',
       repositoryUrl: 'https://github.com/zaarza/restaurant-catalogue',
